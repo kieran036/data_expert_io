90a595fc7240c9731e12391822c35cbd00473ab8
Kieran <79286428+kieran036@users.noreply.github.com>
2024-12-09 01:46:26 +0000

    Add q3 question

---
+++ sql_practice/q3.sql
@@ -0,0 +1,22 @@
+/*
+ Month with the Highest Total Births
+ 
+ 
+ Determine the month with the highest total number of births in the playground.us_birth_stats table. The output should show the month and the total number of births.
+ These are the tables to query for this question:
+ playground.us_birth_stats
+ 
+ year int
+ month int
+ date_of_month int
+ day_of_week int
+ births int
+ 
+ Your answer should include these columns:
+ 
+ month integer
+ total_births integer
+ 
+ Notes:
+ -
+ */
