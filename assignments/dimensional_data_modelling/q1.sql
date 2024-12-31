e2c2f95eef8bfb001cceef6c19d089eba6799ea8
Kieran <79286428+kieran036@users.noreply.github.com>
2024-12-31 22:15:22 +0000

    Add assignment1 q1

---
+++ assignments/dimensional_data_modelling/q1.sql
@@ -0,0 +1,21 @@
+-- 1. **DDL for `actors` table:**
+-- a. An array of struct
+CREATE TYPE films AS (
+    film TEXT,
+    votes INTEGER,
+    rating REAL,
+    filmid TEXT
+);
+-- b. An enumeration of quality_class
+CREATE TYPE quality_class AS ENUM ('star', 'good', 'average', 'bad');
+-- c. 'actors' table with required and relevant fields
+CREATE TABLE actors (
+    actorid TEXT,
+    actor TEXT,
+    -- Array of the `films` type
+    films films [],
+    -- Array of the `quality_class` enum type
+    quality_class quality_class [],
+    is_active BOOLEAN NOT NULL,
+    PRIMARY KEY (actorid)
+);
