e9caee285b6fac7f89936b51ecf4d487e13160c7
Kieran <79286428+kieran036@users.noreply.github.com>
2025-01-01 01:35:19 +0000

    Builds to a1q2 for films column

--- assignments/dimensional_data_modelling/q2.sql
+++ assignments/dimensional_data_modelling/q2.sql
@@ -1,25 +1,43 @@
 -- 2. **Cumulative table generation query:**
--- Build cumulative table from the year before the min year. This acts like a blank query to which you join on the min year which is where data begins.
+-- Build cumulative table from the year before the min year. This is the seed query to which you join on the min year which is where data begins.
 WITH min_year AS (
     SELECT MIN(YEAR)
-    FROM actor_films
+    FROM actor_films;
 ),
 last_year AS (
-    SELECT * -- Last year comes from the cumulative table we're building. For the first build we start with year = min_year - 1
-    FROM actors
-    WHERE current_year = min_year - 1
+    SELECT *
+    FROM actors -- Last year comes from the cumulative actors table we're building. For seed query we start with year = min_year - 1.
+    WHERE current_year = min_year - 1;
 ),
 this_year AS (
-    SELECT * -- This year comes from the data we have in the actor_films table of all the events.
-    FROM actor_films
-    WHERE Year = min_year
-) -- I now change approach to work in smaller steps, follow tutorial side-by-side and starting back from full outer join
+    SELECT *
+    FROM actor_films -- This year comes from actor_films table with all the events.
+    WHERE Year = min_year;
+);
+-- I now change approach to work in smaller steps, follow tutorial side-by-side and starting back from full outer join
 INSERT INTO players
 SELECT COALESCE(ly.actorid, ty.actorid) AS actorid,
     COALESCE(ly.actor, ty.actor) AS actor,
-    -- To add films[]
+    -- COALESCE non-temporal fields
+    CASE
+        WHEN ly.films IS NULL -- No film history for this actor
+        THEN ARRAY [ROW(
+            ty.film,
+            ty.votes,
+            ty.rating,
+            ty.filmid
+        )::films],
+        WHEN ty.films IS NOT NULL -- New film to add
+        THEN ly.films || ARRAY [ROW(
+            ty.film,
+            ty.votes,
+            ty.rating,
+            ty.filmid
+        )::films] ARRAY []
+        ELSE ly.films -- No new film to add
+    END AS films,
     -- To add quality_class[]
-    current_year = min_year,
+    --is_active = current_year = min_year,
     """
     COALESCE(ly.films, ty.films) AS films,
     COALESCE(ly.quality_class, this_year.quality_class) AS quality_class,
