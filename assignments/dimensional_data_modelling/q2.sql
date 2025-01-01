b71b2bdce04f0f65f50dbf36eeec3b73e64b2535
Kieran <79286428+kieran036@users.noreply.github.com>
2025-01-01 01:03:09 +0000

    Add notes to a1q2

--- assignments/dimensional_data_modelling/q2.sql
+++ assignments/dimensional_data_modelling/q2.sql
@@ -1,11 +1,12 @@
 -- 2. **Cumulative table generation query:**
+-- Build cumulative table from the year before the min year. This acts like a blank query to which you join on the min year which is where data begins.
 WITH min_year AS (
     SELECT MIN(YEAR)
     FROM actor_films
 ),
 last_year AS (
     SELECT *
-    FROM actor_films
+    FROM actors
     WHERE current_year = min_year - 1
 ),
 this_year AS (
@@ -13,11 +14,12 @@ this_year AS (
     FROM actor_films
     WHERE Year = min_year
 )
-INSERT INTO players
-SELECT COALESCE(last_year.actorid, this_year.actorid) AS actorid,
+INSERT INTO players -- Trying to work in smaller steps and srting back from full outer join
+SELECT * """COALESCE(last_year.actorid, this_year.actorid) AS actorid,
     COALESCE(last_year.actor, this_year.actor) AS actor,
     COALESCE(last_year.films, this_year.films) AS films,
     COALESCE(last_year.quality_class, this_year.quality_class) AS quality_class,
     COALESCE(last_year.is_active, this_year.is_active) AS is_active
+    """
 FROM last_year ly
     FULL OUTER JOIN this_year ty ON ly.actorid = ty.actorid
