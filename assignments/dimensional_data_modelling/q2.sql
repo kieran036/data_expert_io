7d45557ae8b0361f68fe8cadb0b8efb10b628427
Kieran <79286428+kieran036@users.noreply.github.com>
2025-01-01 01:12:17 +0000

    Builds to a1q2

--- assignments/dimensional_data_modelling/q2.sql
+++ assignments/dimensional_data_modelling/q2.sql
@@ -5,20 +5,24 @@ WITH min_year AS (
     FROM actor_films
 ),
 last_year AS (
-    SELECT *
+    SELECT * -- Last year comes from the cumulative table we're building. For the first build we start with year = min_year - 1
     FROM actors
     WHERE current_year = min_year - 1
 ),
 this_year AS (
-    SELECT *
+    SELECT * -- This year comes from the data we have in the actor_films table of all the events.
     FROM actor_films
     WHERE Year = min_year
-)
-INSERT INTO players -- Trying to work in smaller steps and srting back from full outer join
-SELECT * """COALESCE(last_year.actorid, this_year.actorid) AS actorid,
-    COALESCE(last_year.actor, this_year.actor) AS actor,
-    COALESCE(last_year.films, this_year.films) AS films,
-    COALESCE(last_year.quality_class, this_year.quality_class) AS quality_class,
+) -- I now change approach to work in smaller steps, follow tutorial side-by-side and starting back from full outer join
+INSERT INTO players
+SELECT COALESCE(ly.actorid, ty.actorid) AS actorid,
+    COALESCE(ly.actor, ty.actor) AS actor,
+    -- To add films[]
+    -- To add quality_class[]
+    current_year = min_year,
+    """
+    COALESCE(ly.films, ty.films) AS films,
+    COALESCE(ly.quality_class, this_year.quality_class) AS quality_class,
     COALESCE(last_year.is_active, this_year.is_active) AS is_active
     """
 FROM last_year ly
